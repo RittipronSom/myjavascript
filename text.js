011e814c5cc81f73c8034d15f9410d9ca8d04eba
RittipronSom <6406021621064@fitm.kmutnb.ac.th>
2022-12-30 03:38:25 +0700

    update

---
+++ text.js
@@ -0,0 +1,12 @@
+
+function text1(textdee,callback){
+    let textsee = "real true"+textdee;
+    callback(textsee);
+}
+function text2(textdee){
+    console.log(textdee + "monday");
+}
+text1("hello",text2);
+
+
+
