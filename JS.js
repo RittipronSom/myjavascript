d12c465f8577da920e56af175c66882abde1c8b0
RittipronSom <6406021621064@fitm.kmutnb.ac.th>
2022-12-30 03:06:30 +0700

    update

---
+++ JS.js
@@ -0,0 +1,7 @@
+function equation(result){
+
+    return eval(result);
+}
+
+console.log(equation("1+1"));
+console.log(equation("1+1"));
---
+++ time.js
@@ -0,0 +1,6 @@
+const calculator = (a,b) => a+b;
+const calculator1 = (a,b) => a-b;
+const calculator2 = (a,b) => a*b;
+console.log(calculator(1,2));
+console.log(calculator1(1,2));
+console.log(calculator2(1,2));
